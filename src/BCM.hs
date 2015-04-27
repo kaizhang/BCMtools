6dd9766535ba79d5aa3a2de074f60024a67e0253
Kai Zhang <kai@kzhang.org>
2015-04-27 11:52:47 -0700

    add functions for creating, opening, saving, and closing contact maps

--- src/BCM.hs
+++ src/BCM.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE KindSignatures #-}
 module BCM where
 
 import Control.Monad (when, guard)
@@ -8,13 +8,13 @@ import Control.Monad.IO.Class (MonadIO(..))
 import qualified Data.ByteString.Lazy as L
 import qualified Data.ByteString.Char8 as B
 import qualified Data.HashMap.Strict as M
-import Data.Binary
 import Data.Binary.Put
 import Data.Binary.Get
 import Data.Conduit
 import qualified Data.Conduit.List as CL
 import System.IO
 import Data.List (foldl')
+import Data.Word (Word32)
 import Text.Printf (printf)
 
 import qualified BCM.DiskMatrix as DM
@@ -23,67 +23,27 @@ import qualified BCM.IOMatrix as IOM
 -- contact map binary format
 -- 4 bytes magic + 4 bytes Int (step) + chroms + 1 bytes (reserve) + matrix
 
-data ContactMap m = ContactMap
+data ContactMap (m :: (* -> *) -> * -> *) t a = ContactMap
     { _rowLabels :: M.HashMap B.ByteString (Int, Int)
     , _colLabels :: M.HashMap B.ByteString (Int, Int)
     , _resolution :: Int
-    , _matrix :: m
+    , _matrix :: m t a
+    , _handle :: Handle
     }
+
 
 contact_map_magic :: Word32
 contact_map_magic = 0x9921ABF0
 
-instance Binary m => Binary (ContactMap m) where
-    put (ContactMap rowChr colChr res mat) = do
-        putWord32le contact_map_magic
-        putWord32le . fromIntegral $ res
-        put rowAndcol
-        putWord8 0
-        put mat
-      where
-        rowAndcol = L.concat [rows, "\0", cols, "\0"]
-        rows = encodeLab . M.toList $ rowChr
-        cols = encodeLab . M.toList $ colChr
-        encodeLab xs = L.concat $ concatMap (\(chr, (a,b)) ->
-            [L.fromStrict chr, "\0", DM.toByteString a, DM.toByteString b]) xs
-
-    get = do
-        magic <- getWord32le
-        guard $ magic == contact_map_magic
-        res <- fromIntegral <$> getWord32le
-        rows <- M.fromList <$> getChrs []
-        cols <- M.fromList <$> getChrs []
-        _ <- getWord8
-        mat <- get
-        return $ ContactMap rows cols res mat
-      where
-        getChrs acc = do
-            chr <- L.toStrict <$> getLazyByteStringNul
-            if B.null chr
-               then return acc
-               else do
-                   a <- fromIntegral <$> getWord64le
-                   b <- fromIntegral <$> getWord64le
-                   getChrs $ (chr, (a, b)) : acc
-
-toContactMap :: (IOM.IOMatrix mat t Double, MonadIO io, m ~ mat t Double)
-             => FilePath
-             -> [(B.ByteString, Int)]
-             -> [(B.ByteString, Int)]
-             -> Int
-             -> Maybe Int
-             -> Sink (((B.ByteString, Int), (B.ByteString, Int)), Double) io (ContactMap m)
-toContactMap fl rowChr colChr res len = do
-    -- write header to file
-    h <- liftIO $ do
-        handle <- openFile fl ReadWriteMode
-        L.hPut handle $ runPut $ putWord32le contact_map_magic
-        L.hPut handle $ runPut $ putWord32le $ fromIntegral res
-        DM.hWrite1 handle offset
-        L.hPut handle $ runPut $ putWord8 0
-        L.hPut handle rows
-        L.hPut handle cols
-        return handle
+createContactMap :: (IOM.IOMatrix m t Double, MonadIO io)
+                 => FilePath
+                 -> [(B.ByteString, Int)]
+                 -> [(B.ByteString, Int)]
+                 -> Int
+                 -> Maybe Int
+                 -> Sink (((B.ByteString, Int), (B.ByteString, Int)), Double) io (ContactMap m t Double)
+createContactMap fl rowChr colChr res len = do
+    h <- liftIO $ openFile fl ReadWriteMode
 
     let source  = CL.mapM $ \( ((chr1,i), (chr2,j)), v ) -> do
             let (p1, s1) = M.lookupDefault undefined chr1 rLab
@@ -97,18 +57,66 @@ toContactMap fl rowChr colChr res len = do
     liftIO $ hSeek h AbsoluteSeek (fromIntegral offset)
     m <- source $= IOM.hCreateMatrix h (r,c) len
 
-    return $ ContactMap rLab cLab res m
+    return $ ContactMap rLab cLab res m h
   where
     r = foldl' (\acc (_,x) -> acc + (x - 1) `div` res + 1)  0 rowChr
     c = foldl' (\acc (_,x) -> acc + (x - 1) `div` res + 1)  0 colChr
     rLab = mkLabels rowChr res
     cLab = mkLabels colChr res
-    encodeLab xs = L.intercalate "" (map (\(chr, s) -> L.fromStrict chr `L.append` "\0" `L.append` DM.toByteString s) xs) `L.append` "\0"
-    rows = encodeLab rowChr
-    cols = encodeLab colChr
-    nrow = fromIntegral $ L.length rows
-    ncol = fromIntegral $ L.length cols
-    offset = 4 + 4 + 8 + 1 + nrow + ncol :: Int
+    nByte x = let n1 = foldl' (+) 0 $ map B.length $ fst $ unzip x
+                  n2 = n - 1
+                  n3 = 16 * n
+                  n = length x
+               in n1 + n2 + n3
+    offset = 4 + 4 + nByte rowChr + nByte colChr + 2 + 1
+
+saveContactMap :: IOM.IOMatrix m t a => ContactMap m t a -> IO ()
+saveContactMap (ContactMap rowChr colChr res mat handle) = do
+        hSeek handle AbsoluteSeek 0
+        L.hPutStr handle . runPut . putWord32le $ contact_map_magic
+        L.hPutStr handle . runPut . putWord32le . fromIntegral $ res
+        L.hPutStr handle rowAndcol
+        L.hPutStr handle . runPut . putWord8 $ 0
+        IOM.hSaveMatrix handle mat
+      where
+        rowAndcol = L.concat [rows, "\0", cols, "\0"]
+        rows = encodeLab . M.toList $ rowChr
+        cols = encodeLab . M.toList $ colChr
+        encodeLab xs = L.concat $ concatMap (\(chr, (a,b)) ->
+            [L.fromStrict chr, "\0", DM.toByteString a, DM.toByteString b]) xs
+
+openContactMap :: (IOM.IOMatrix m t a, MonadIO io) => FilePath -> io (ContactMap m t a)
+openContactMap fl = liftIO $ do
+    h <- openFile fl ReadWriteMode
+
+    magic <- runGet getWord32le <$> L.hGet h 4
+    guard $ magic == contact_map_magic
+    res <- fromIntegral . runGet getWord32le <$> L.hGet h 4
+
+    rows <- M.fromList <$> getChrs [] h
+    cols <- M.fromList <$> getChrs [] h
+    _ <- runGet getWord8 <$> L.hGet h 1
+    mat <- IOM.hReadMatrix h
+    return $ ContactMap rows cols res mat h
+  where
+    getChrs acc h = do
+        chr <- getByteStringNul h
+        if B.null chr
+           then return acc
+           else do
+               a <- fromIntegral . runGet getWord64le <$> L.hGet h 8
+               b <- fromIntegral . runGet getWord64le <$> L.hGet h 8
+               getChrs ((chr, (a, b)) : acc) h
+    getByteStringNul h = B.concat <$> go []
+      where
+        go acc = do
+            x <- B.hGet h 1
+            case x of
+                "\0" -> return acc
+                _ -> go $ acc ++ [x]
+
+closeContactMap :: ContactMap m t a -> IO ()
+closeContactMap cm = hClose $ _handle cm
 
 
 -------------------------------------------------------------------------------
